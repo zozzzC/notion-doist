cd1d9d4c354933721ac6ed2d05e1e9bce4b89d3a
zozzzC <czlc05edu@gmail.com>
2025-02-26 13:31:36 +1300

    created auth methods for both todoist and notion

---
+++ src/todoist/auth.py
@@ -0,0 +1,10 @@
+import os
+from todoist_api_python.api import TodoistAPI
+
+def auth() -> TodoistAPI:
+    try:
+       api = TodoistAPI(os.environ(["TODOIST_TOKEN"]));
+       return api;
+    except:
+        print("Todoist Token is not initialized.")
+        
