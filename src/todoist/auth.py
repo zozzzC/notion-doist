addeb807737eecfb33895ed9f9f4a1017d34d67f
zozzzC <czlc05edu@gmail.com>
2025-02-26 13:37:42 +1300

    format

--- src/todoist/auth.py
+++ src/todoist/auth.py
@@ -1,10 +1,10 @@
 import os
 from todoist_api_python.api import TodoistAPI
 
+
 def auth() -> TodoistAPI:
     try:
-       api = TodoistAPI(os.environ(["TODOIST_TOKEN"]));
-       return api;
+        api = TodoistAPI(os.environ(["TODOIST_TOKEN"]))
+        return api
     except:
         print("Todoist Token is not initialized.")
-        
