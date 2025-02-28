c5039ecd7aae22f26661f2d0a67a83f46fb4fd2b
zozzzC <czlc05edu@gmail.com>
2025-02-28 14:17:24 +1300

    created formatToDoIstDate that allows todoist dates to reformat into ISO format

--- src/todoist/helpers/formatToDoIstDate.py
+++ src/todoist/helpers/formatToDoIstDate.py
@@ -1,2 +1,13 @@
-def formatToDoIstDate(startDate: str, endDate: str | None):
-    print("format doIst Date")
+import datetime
+
+
+def formatToDoIstDate(due: str, dt: str | None):
+    dueReformat = datetime.datetime.strptime(due, "YYYY-MM-DD").isoformat()
+
+    if dt:
+        dtReformat = datetime.datetime.strptime(
+            dt, "yyyy-mm-ddThh:mm:ss.msZ"
+        ).isoformat()
+        return dueReformat, dtReformat
+
+    return dueReformat
