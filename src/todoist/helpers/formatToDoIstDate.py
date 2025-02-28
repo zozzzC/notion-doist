2edbd956ba9515a0d2d6c4ca6c66f0f2f636fe3b
zozzzC <czlc05edu@gmail.com>
2025-02-28 14:28:54 +1300

    changed timezone of due and datetime strings in formatToDoIstDate

--- src/todoist/helpers/formatToDoIstDate.py
+++ src/todoist/helpers/formatToDoIstDate.py
@@ -1,13 +1,20 @@
 import datetime
+import pytz
 
 
-def formatToDoIstDate(due: str, dt: str | None):
-    dueReformat = datetime.datetime.strptime(due, "YYYY-MM-DD").isoformat()
+def formatToDoIstDate(due: str, dt: str | None, timezone: str):
+    utcTimezone = pytz.timezone("UTC")
+    myTimezone = pytz.timezone(timezone)
+
+    dueReformat = utcTimezone.localize(
+        datetime.datetime.strptime(due, "YYYY-MM-DD").isoformat()
+    ).astimezone(myTimezone)
 
     if dt:
-        dtReformat = datetime.datetime.strptime(
-            dt, "yyyy-mm-ddThh:mm:ss.msZ"
-        ).isoformat()
+        dtReformat = utcTimezone.localize(
+            datetime.datetime.strptime(dt, "yyyy-mm-ddThh:mm:ss.msZ").isoformat()
+        ).astimezone(myTimezone)
+
         return dueReformat, dtReformat
 
     return dueReformat
