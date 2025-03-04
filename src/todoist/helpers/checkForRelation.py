b1061aee2106dc213d63ac96e51d0fce49dd8d8a
zozzzC <czlc05edu@gmail.com>
2025-03-05 09:51:52 +1300

    removed tasks from checkForRelation method

--- src/todoist/helpers/checkForRelation.py
+++ src/todoist/helpers/checkForRelation.py
@@ -3,28 +3,11 @@ from src.todoist.syncTasks import tasksType
 
 
 def checkForRelation(
-    t: dict[
-        "content":str,
-        "duration" : str | None,
-        "duration_unit" : str | None,
-        "datetime" : str | None,
-        "description":str,
-        "parent_id" : str | None,
-        "is_completed":bool,
-        "labels" : list[str | None],
-        "timezone" : str | None,
-        "priority":int,
-        "project_id" : str | None,
-        "section_id" : str | None,
-        "due" : str | None,
-        "recurring":bool,
-        "parent_id" : str | None,
-    ],
     reformatted_tasks: dict[str : dict[tasksType]],
 ):
     print(
         "this must check for a relation for some task, and if so, then we must then add this relation into notion"
     )
-
-    print("reformatted task todoist id: " + reformatted_tasks[t])
-    syncRelations(reformatted_tasks[t], t.get("parent_id"))
+    for t in reformatted_tasks:
+        print("reformatted task todoist id: " + reformatted_tasks[t])
+        syncRelations(reformatted_tasks[t], t.get("parent_id"))
