120cd724f6c877e197c379606e96515ca3b0406c
zozzzC <czlc05edu@gmail.com>
2025-07-07 14:31:39 +1200

    create updateDoIstTask function.

---
+++ src/notion/helpers/updateDoIstTask.py
@@ -0,0 +1,128 @@
+from src.notion.types.NotionTypes import NotionPropsType
+from src.notion.types.PagesTypes import PagesType
+from pprint import pprint
+from src.notion.helpers.getParentId import getParentId
+from src.todoist.auth import doIstAuth
+from src.notion.auth import notionAuth
+from datetime import datetime
+from src.notion.helpers.getTime import getTime
+from src.notion.helpers.convertPriority import convertPriority
+from src.notion.helpers.section.createSectionId import createSectionId
+from src.notion.helpers.section.getSectionId import getSectionId
+from src.notion.helpers.project.createProjectId import createProjectId
+from src.notion.helpers.project.getProjectId import getProjectId
+from todoist_api_python.models import Task
+
+
+def updateDoIstTask(pageId: str, page: dict[PagesType], doist_parent_id: str | None):
+    api = doIstAuth()
+    client = notionAuth()
+    content = page["Name"]
+
+    date = page["Date"]
+    start_date = None
+    start_time = None
+    end_date = None
+
+    due_date = None
+    due_datetime = None
+    duration = None
+    duration_unit = None
+
+    if date != None:
+        # then there must be a start date (at least.)
+        start_date = date["start"]
+        # check if start_date has a time.
+        start_time = getTime(start_date)
+
+        if start_time != None:
+            due_date = datetime.fromisoformat(start_date).date()
+        else:
+            due_datetime = datetime.fromisoformat(start_date)
+
+        end_date = date["end"]
+
+        if end_date != None:
+            # then there is a duration -- this could be either a day duration (if start and end dates do not have a time field)
+            # OR it could be a time duration
+            if start_time != None:
+                # in this case its a time duration
+                duration = (
+                    datetime.fromisoformat(end_date)
+                    - datetime.fromisoformat(start_date)
+                ).total_seconds() / 3600
+                duration_unit = "minute"
+                print("time duration")
+            else:
+                # in this case it is a day duration
+                duration = (
+                    datetime.fromisoformat(end_date).date()
+                    - datetime.fromisoformat(start_date).date()
+                ).days
+                duration_unit = "day"
+                print("day duration")
+            # TODO fix duration, must be string, but right now its a timedelta obj
+
+    deadline = page["Deadline"]
+
+    deadline_date = None
+
+    if deadline != None:
+        deadline_date = datetime.fromisoformat(deadline["start"]).date().isoformat()
+
+    labels = page["Label"]
+
+    priority = convertPriority(page["Priority_Level"])
+
+    # for projects, we first check if the project exists, if not, then create the project.
+
+    project_id = getProjectId("Inbox")
+
+    if page["Project"] != None:
+        project_id = getProjectId(page["Project"])
+        if project_id == None:
+            project_id = createProjectId(page["Project"])
+
+    # for sections, we need to do the same as the projects
+
+    section_id = None
+
+    if page["Section"] != None:
+        section_id = getSectionId(page["Section"])
+
+        print(section_id)
+
+        if project_id == getProjectId("Inbox"):
+            print(
+                "Page "
+                + pageId
+                + " has section but no project ID, so section is not being synced."
+            )
+            section_id = None
+        elif section_id == None and project_id != getProjectId("Inbox"):
+            section_id = createSectionId(page["Section"], project_id)
+
+    if doist_parent_id != None:
+        print("Doist parent id is: " + doist_parent_id)
+
+    task_id = page["ToDoistId"]
+
+    api.update_task(
+        task_id=task_id,
+        content=content,
+        labels=labels,
+        priority=priority,
+        due_date=due_date,
+        due_datetime=due_datetime,
+        duration=duration,
+        duration_unit=duration_unit,
+        deadline_date=deadline_date,
+    )
+
+    api.move_task(
+        task_id=task_id,
+        project_id=project_id,
+        section_id=section_id,
+        parent_id=doist_parent_id,
+    )
+
--- src/notion/syncPages.py
+++ src/notion/syncPages.py
@@ -10,6 +10,7 @@ from src.notion.helpers.ReformatPage import ReformatPages
 from src.notion.helpers.createDoIstTask import createDoIstTask
 from queue import Queue
 from src.notion.helpers.getParentId import getParentId
+from src.notion.helpers.updateDoIstTask import updateDoIstTask
 
 
 def syncPages(client: Client, data: any):
@@ -51,6 +52,8 @@ def syncPages(client: Client, data: any):
             },
         }
     )
+    reformatUpdatePages = ReformatPages()
+    reformatUpdatePages.reformatPages(update_pages)
 
     reformatCompletePages = ReformatPages()
     reformatCompletePages.reformatPages(complete_pages)
@@ -58,16 +61,13 @@ def syncPages(client: Client, data: any):
     reformatNewPages = ReformatPages()
     reformatNewPages.reformatPages(new_pages)
 
-    reformatUpdatePages = ReformatPages()
-    reformatUpdatePages.reformatPages(update_pages)
-
     # notion cache contains potentially completed and potentially updated pages.
-    with open(os.getcwd() + "/test/notionpage.json", "r") as f:
-        cache_pages: dict[str : dict[pagesType]] = json.load(f)
+    with open(os.getcwd() + "/test/notionPage.json", "r") as f:
+        cache_pages: dict[str : dict[PagesType]] = json.load(f)
 
     # first we get all the new pages and add them into todoist. then we get the todoist id back and store this into the associated notion page.
 
-    needs_parent_id: Queue[dict[pagesType]] = Queue()
+    needs_parent_id: Queue[dict[PagesType]] = Queue()
 
     for page in reformatNewPages.reformatted:
         if reformatNewPages.reformatted[page]["ParentId"] != None:
@@ -90,7 +90,6 @@ def syncPages(client: Client, data: any):
                 "Successfully added task " + reformatNewPages.reformatted[page]["Name"]
             )
 
-    print("Now looping through create queue...")
     while needs_parent_id.empty() == False:
         page = needs_parent_id.get()
         doist_parent_id = getParentId(
@@ -105,13 +104,68 @@ def syncPages(client: Client, data: any):
                 "Successfully added task " + reformatNewPages.reformatted[page]["Name"]
             )
 
-    # for page in reformatUpdatePages.reformatted:
+    # if there is no notion cache (dict is empty), then we have to add all into ticktick first, otherwise, we add it back again to the existing cache.
+    if len(cache_pages) == 0:
+        with open(os.getcwd() + "/test/notionPage.json", "w") as f:
+            pprint(reformatNewPages.reformatted)
+            json.dump(reformatNewPages.reformatted, f)
+            f.close()
+            print("Saving Notion pages...")
+            return
+    # if there IS a notion cache, we want to combine the notion cache and reformatNewPages.
+    else:
+        with open(os.getcwd() + "/test/notionPage.json", "w") as f:
+            cache_pages.update(reformatNewPages.reformatted)
+            json.dump(cache_pages, f)
+            f.close()
+            print("Cache Pages found. Adding new pages to the updated pages cache.")
 
-    # # if there is no notion cache (dict is empty), then we have to add all into ticktick first, otherwise, we add it back again to the existing cache.
-    # if len(cache_pages) == 0:
-    #     with open(os.getcwd() + "/test/notionpage.json", "w") as f:
-    #         print("Saving Notion pages...")
-    #         json.dump(reformatNewPages.reformatted, f)
-    #         f.close()
-
+    pprint(reformatUpdatePages)
     # get all the pages that already have a doist ID and check with the cache if they were updated.
+    for page in reformatUpdatePages.reformatted:
+        # determine if there are any changes.
+        # first we look up the corresponding page in our cache.
+
+        cache_page = cache_pages[page]
+
+        # now loop through the properties.
+
+        for prop in reformatUpdatePages.reformatted[page]:
+            if (
+                reformatUpdatePages.reformatted[page][prop] != cache_page[prop]
+            ):  # in case of a property mismatch, we update.
+                print(
+                    "Task "
+                    + reformatUpdatePages.reformatted[page]["Name"]
+                    + " change was found. Attempting to update..."
+                )
+                # there's a mismatch, so the given page must be updated.
+                if reformatUpdatePages.reformatted[page]["ParentId"] != None:
+                    doist_parent_id = getParentId(
+                        reformatUpdatePages.reformatted[page]["ParentId"],
+                        page,
+                        needs_parent_id,
+                    )
+                    if doist_parent_id != None:
+                        updateDoIstTask(
+                            page, reformatUpdatePages.reformatted[page], doist_parent_id
+                        )
+                    print(
+                        "Successfully updated added task "
+                        + reformatUpdatePages.reformatted[page]["Name"]
+                    )
+                else:
+                    doist_parent_id = None
+                    updateDoIstTask(
+                        page, reformatUpdatePages.reformatted[page], doist_parent_id
+                    )
+                    print(
+                        "Successfully updated task "
+                        + reformatUpdatePages.reformatted[page]["Name"]
+                    )
+
+    with open(os.getcwd() + "/test/notionPage.json", "w") as f:
+        cache_pages.update(reformatUpdatePages.reformatted)
+        json.dump(cache_pages, f)
+        f.close()
+        print("Cache Pages found. Adding updated pages to the updated pages cache.")
